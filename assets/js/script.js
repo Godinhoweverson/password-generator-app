317d193b094c16c0cecf2f75e52bbec812cbe0ed
Weverson Godinho <weverson.godinho@icloud.com>
2024-09-13 14:51:19 +0000

    create generate function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,99 @@
 const sliderEl = document.querySelector("#range")
 const sliderValue = document.querySelector(".value")
-const checkboxes = document.getElementsByName('selected');
-
+const btnGenerate = document.getElementById('btn-generate')
 
 
 // RANGE,  GET VALUE FROM RANGE INPUT
 let tempSliderValue = 0
 sliderEl.addEventListener("input", (event) => {
   tempSliderValue = event.target.value; 
-  
   sliderValue.textContent = tempSliderValue;
   
   const progress = (tempSliderValue / sliderEl.max) * 100;
 
   sliderEl.style.background = `linear-gradient(to right, #A4FFAF ${progress}%, #18171F ${progress}%)`;
+
 })
 
-// Get value from checkbox to know what kind of charcters the user wanna 
+
+// Get value from checkbox to know what kind of charcters will be sort on password 
 const getValue = () =>{
-  const c = document.getElementsByName('selected');
+  const selected = document.getElementsByName('selected');
   let result = [];
-  for (let i = 0; i < c.length; i++){
-    if(c[i].checked){
-      result.push(Number(c[i].value))
+  for (let i = 0; i < selected.length; i++){
+    if(selected[i].checked){
+      result.push(Number(selected[i].value))
     }
   }
-  divideIntoRandomArrays(tempSliderValue,result.length)
+
+  generate(result) // pass the value as argument to bntGenerate function
 }
 
-//Used to define how characters for wich type
+//Used to define how many characters for which type
 const divideIntoRandomArrays = function (elementsCount, arraysCount) {
-  // Generate an array with numbers 1 to elementsCount
-  const array = Array.from({ length: elementsCount }, (_, i) => i + 1);
-  
-  // Shuffle the array using Fisher-Yates algorithm for randomness
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+  if(arraysCount > 0){
+        // Generate an array with numbers 1 to elementsCount
+    const array = Array.from({ length: elementsCount }, (_, i) => i + 1);
+
+    // Shuffle the array using Fisher-Yates algorithm for randomness
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    // Create arraysCount empty arrays
+    const arrays = Array.from({ length: arraysCount }, () => []);
+
+    // Distribute the shuffled elements into the arrays
+    for (let i = 0; i < array.length; i++) {
+        arrays[i % arraysCount].push(array[i]);
+    }
+    // if(arrays.length === 1) {
+    //   getValueUpperCase(arrays[0].length)
+    //   }else if(arrays.length === 2){
+    //   getValueUpperCase(arrays[0].length)
+    //   getValueLowerCase(arrays[1].length)
+    //   }else if(arrays.length === 3){
+    //   getValueUpperCase(arrays[0].length)
+    //   getValueLowerCase(arrays[1].length)
+    //   getValueNumbers(arrays[2].length)
+    //   }else if(arrays.length === 4){
+    //   getValueUpperCase(arrays[0].length)
+    //   getValueLowerCase(arrays[1].length)
+    //   getValueNumbers(arrays[2].length)
+    //   getValueSymbols(arrays[3].length)
+    // }
+    console.log(arrays[0])
+    // console.log(arrays[1])
+    // console.log(arrays[2])
+    // console.log(arrays[3])
+
   }
-
-  // Create arraysCount empty arrays
-  const arrays = Array.from({ length: arraysCount }, () => []);
-
-  // Distribute the shuffled elements into the arrays
-  for (let i = 0; i < array.length; i++) {
-      arrays[i % arraysCount].push(array[i]);
-  }
-
-if(arrays.length === 1) {
-  getValueUpperCase(arrays[0].length)
-}else if(arrays.length === 2){
-  getValueUpperCase(arrays[0].length)
-  getValueLowerCase(arrays[1].length)
-}else if(arrays.length === 3){
-  getValueUpperCase(arrays[0].length)
-  getValueLowerCase(arrays[1].length)
-  getValueNumbers(arrays[2].length)
-}else if(arrays.length === 4){
-  getValueUpperCase(arrays[0].length)
-  getValueLowerCase(arrays[1].length)
-  getValueNumbers(arrays[2].length)
-  getValueSymbols(arrays[3].length)
-}
+ 
 };
+
+const generate = (result) =>{
+
+  btnGenerate.addEventListener('click', ()=>{
+    divideIntoRandomArrays(tempSliderValue,result.length)
+  })
+}
+
 
 const characterLength = function (boxSelected){
   console.log(chLength)
   }
 
 const getValueUpperCase = (c) => {
-  const upperCase = []
+  let upperCase = []
   for(let i = 0; i < c; i++){
     const min = 65;
     const max = 90;
    upperCase.push(Math.floor(Math.random() * (max - min) + min));
   }
-console.log(upperCase)
+  const result = upperCase; // Store result to return
+  upperCase = []; // Reset the array (not strictly necessary since it's local)
+
 }
 const getValueLowerCase = (c) => {
   console.log(c)
